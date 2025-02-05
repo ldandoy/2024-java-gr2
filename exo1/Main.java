a34a676ce2b904c748cc7b47bed0ef492d9418e7
ldandoy <ldandoy@overconsulting.net>
2025-02-05 15:24:41 +0100

    Ajout de l exo 1

---
+++ exo1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Voiture voiture = new Voiture();
+        voiture.marque = "Toyota";
+        voiture.modele = "Corolla";
+        voiture.couleur = "Rouge";
+
+        voiture.demarrer();
+    }
+}
---
+++ exo1/Voiture.java
@@ -0,0 +1,20 @@
+// Voiture.java
+public class Voiture {
+    // Attributs
+    String marque;
+    String modele;
+    String couleur;
+
+    // Méthodes
+    void demarrer() {
+        System.out.println("La voiture démarre");
+    }
+	
+	void accelerer() {
+        System.out.println("La voiture accelère");
+    }
+
+    void freiner() {
+        System.out.println("La voiture freine");
+    }
+}
